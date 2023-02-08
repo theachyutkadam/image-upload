8520e8982d9adb9650739a236375df17c34f5f5a
achyutkadamacl <achyut.k@acldigital.com>
2023-02-08 12:31:23 +0530

    Multiple image upload

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,11 @@
+class MessagesController < ApplicationController
+  def create
+    message = Message.create!(message_params)
+    redirect_to message
+  end
+
+  private
+    def message_params
+      params.require(:message).permit(:title, :content, images: [])
+    end
+end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,7 @@
 class Message < ApplicationRecord
-  has_many_attached :images
+  # has_many_attached :images
+  # has_many_attached :images, service: :s3
+  has_many_attached :images do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+  end
 end
